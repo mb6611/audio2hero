73e4e401cbc4fe113ef94e9b61b7297d8f1ce15d
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2024-05-08 22:54:17 -0400

    post processors

---
+++ audio2hero.py
@@ -0,0 +1,52 @@
+import argparse
+import sys
+import librosa
+from transformers import Pop2PianoForConditionalGeneration, Pop2PianoProcessor, Pop2PianoTokenizer, Pop2PianoConfig
+import pretty_midi
+from transformers import AutoConfig
+from types import MethodType
+from model_generate import generate
+import torch
+from post_processor import post_process
+import tempfile
+
+
+
+if __name__=="__main__":
+  args = sys.argv[1:]
+  song_path = args[0]
+  output_dir = args[1]
+
+  print("Loading Model...")
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  model = Pop2PianoForConditionalGeneration.from_pretrained("./models/audio2hero_adafactor_340").to(device)
+  model.eval()
+  processor = Pop2PianoProcessor.from_pretrained("sweetcocoa/pop2piano")
+  tokenizer = Pop2PianoTokenizer.from_pretrained("sweetcocoa/pop2piano")
+
+  print("Processing Song...")
+  # load an example audio file and corresponding ground truth midi file
+  audio, sr = librosa.load(song_path, sr=44100)  # feel free to change the sr to a suitable value.
+  inputs = processor(audio=audio, sampling_rate=sr, return_tensors="pt")
+
+
+  # generate model output
+  print("Generating output...")
+  model.generation_config.output_logits = True
+  model.generation_config.return_dict_in_generate = True
+  model_output = model.generate(inputs["input_features"].to(device))
+
+  tokenizer_output = processor.batch_decode(
+          token_ids=model_output.sequences.cpu(),
+          feature_extractor_output=inputs
+      )
+
+  # save to temp file
+  temp_dir = tempfile.TemporaryDirectory()
+  tokenizer_output["pretty_midi_objects"][0].write(f"{temp_dir.name}/temp.mid")
+
+  print("Post Processing...")
+  post_process(song_path, f"{temp_dir.name}/temp.mid", output_dir)
+
+  temp_dir.cleanup()
+  print("Done.")
--- post_processor.py
+++ post_processor.py
@@ -7,74 +7,128 @@ import configparser
 import os
 import shutil
 import random
+from mergeoggs import merge
 
-output_dir = "./clonehero"
 
-# for file in os.listdir("./processed/piano_midi"):
-# song_name = ".".join(file.split(".")[0:-1])
-song_name = "Dire Straits - Sultans of Swing"
-# song_name = "Aerosmith - Same Old Song & Dance"
-print(song_name)
 
-if not os.path.exists(f"{output_dir}/{song_name}"):
-    os.makedirs(f"{output_dir}/{song_name}")
+def post_process(song_path, midi_path, output_dir):
+    song_name = song_path.split("/")[-1]
+    song_name = ".".join(song_name.split(".")[0:-1])
 
-# copy over song
-# shutil.copy(f"./processed/audio/{song_name}.ogg", f"{output_dir}/{song_name}/song.ogg")
-shutil.copy(f"./{song_name}.ogg", f"{output_dir}/{song_name}/song.ogg")
+    if not os.path.exists(f"{output_dir}/{song_name}"):
+        os.makedirs(f"{output_dir}/{song_name}")
 
-# notes = pretty_midi.PrettyMIDI(f"./processed/piano_midi/{file}")
-notes = pretty_midi.PrettyMIDI(f"./sultans_ada.mid")
-# notes = pretty_midi.PrettyMIDI(f"./{song_name}.mid")
+    merge(f"{output_dir}/{song_name}/song.ogg", song_path)
 
-output = copy.deepcopy(notes)
-output.instruments = []
-output.instruments.append(pretty_midi.Instrument(0, name="PART GUITAR"))
-last_start = 0
+    notes = pretty_midi.PrettyMIDI(midi_path)
 
-note_times = [note.start for note in notes.instruments[0].notes if note.pitch != 78]
+    output = copy.deepcopy(notes)
+    output.instruments = []
+    output.instruments.append(pretty_midi.Instrument(0, name="PART GUITAR"))
 
-total = 0
-outofrange = 0
-for index,note in enumerate(notes.instruments[0].notes):
-    time_start = note.start
-    # if time_start == last_start:
-    #     continue
-    # if index % 2 != 0:
-    #     continue
-    total+=1
-    if note.pitch not in [71,72,73,74,75, 78]:
-        outofrange+=1
-    last_start = time_start
-    # new_pitch = 71 + note.pitch % 5
-    new_pitch = note.pitch
-    duration = note.end - note.start
-    end = note.start + duration if duration > 0.5 else note.start + 0.1
-    new_note = pretty_midi.Note(velocity=100, pitch=new_pitch, start=note.start, end=end)
+    note_times = [note.start for note in notes.instruments[0].notes if note.pitch != 78]
+    for index,note in enumerate(notes.instruments[0].notes):
+        if note.pitch not in [72,73,74,75, 78]:
+            continue
 
-    # if strum
-    if note.pitch == 78 and note.start not in note_times:
-        extra_note = pretty_midi.Note(velocity=100, pitch=random.randint(71,75), start=note.start, end=end)
-        output.instruments[0].notes.append(extra_note)
-    # strum = pretty_midi.Note(velocity=100, pitch=78, start=note.start, end=end)
-    output.instruments[0].notes.append(new_note)
-    # output.instruments[0].notes.append(strum)
+        new_pitch = note.pitch
+        duration = note.end - note.start
+        end = note.start + duration if duration > 0.5 else note.start + 0.1
+        new_note = pretty_midi.Note(velocity=100, pitch=new_pitch, start=note.start, end=end)
 
-print(f"Total notes: {total}")
-print(f"Out of range notes: {outofrange}")
+        # add random note if solo strum
+        if note.pitch == 78 and note.start not in note_times:
+            extra_note = pretty_midi.Note(velocity=100, pitch=random.randint(72,75), start=note.start, end=end)
+            output.instruments[0].notes.append(extra_note)
 
-output.write(f"{output_dir}/{song_name}/notes.mid")
+        output.instruments[0].notes.append(new_note)
 
-output = mido.MidiFile(f"{output_dir}/{song_name}/notes.mid")
-output.tracks[1].pop(1)
-output.save(f"{output_dir}/{song_name}/notes.mid")
 
-# write ini file
-config = configparser.ConfigParser()
-config.read('./song.ini')
-config.set("song", "name", song_name.split(" - ")[1])
-config.set("song", "artist", song_name.split(" - ")[0])
-config.set("song", "charter", "Tim and Matthew")
+    output.write(f"{output_dir}/{song_name}/notes.mid")
 
-with open(f"{output_dir}/{song_name}/song.ini", 'w') as configfile:    # save
-    config.write(configfile)
+    output = mido.MidiFile(f"{output_dir}/{song_name}/notes.mid")
+    output.tracks[1].pop(1)
+    output.save(f"{output_dir}/{song_name}/notes.mid")
+
+    # write ini file
+    config = configparser.ConfigParser()
+    config.read('./song.ini')
+    config.set("song", "name", song_name.split(" - ")[1])
+    config.set("song", "artist", song_name.split(" - ")[0])
+    config.set("song", "charter", "Tim and Matthew")
+
+    with open(f"{output_dir}/{song_name}/song.ini", 'w') as configfile:    # save
+        config.write(configfile)
+
+
+
+# output_dir = "./clonehero"
+
+# # for file in os.listdir("./processed/piano_midi"):
+# # song_name = ".".join(file.split(".")[0:-1])
+# song_name = "Dire Straits - Sultans of Swing"
+# # song_name = "Aerosmith - Same Old Song & Dance"
+# print(song_name)
+
+# if not os.path.exists(f"{output_dir}/{song_name}"):
+#     os.makedirs(f"{output_dir}/{song_name}")
+
+# # copy over song
+# # shutil.copy(f"./processed/audio/{song_name}.ogg", f"{output_dir}/{song_name}/song.ogg")
+# shutil.copy(f"./{song_name}.ogg", f"{output_dir}/{song_name}/song.ogg")
+
+# # notes = pretty_midi.PrettyMIDI(f"./processed/piano_midi/{file}")
+# notes = pretty_midi.PrettyMIDI(f"./sultans_ada.mid")
+# # notes = pretty_midi.PrettyMIDI(f"./{song_name}.mid")
+
+# output = copy.deepcopy(notes)
+# output.instruments = []
+# output.instruments.append(pretty_midi.Instrument(0, name="PART GUITAR"))
+# last_start = 0
+
+# note_times = [note.start for note in notes.instruments[0].notes if note.pitch != 78]
+
+# total = 0
+# outofrange = 0
+# for index,note in enumerate(notes.instruments[0].notes):
+#     time_start = note.start
+#     # if time_start == last_start:
+#     #     continue
+#     # if index % 2 != 0:
+#     #     continue
+#     total+=1
+#     if note.pitch not in [71,72,73,74,75, 78]:
+#         outofrange+=1
+#     last_start = time_start
+#     # new_pitch = 71 + note.pitch % 5
+#     new_pitch = note.pitch
+#     duration = note.end - note.start
+#     end = note.start + duration if duration > 0.5 else note.start + 0.1
+#     new_note = pretty_midi.Note(velocity=100, pitch=new_pitch, start=note.start, end=end)
+
+#     # if strum
+#     if note.pitch == 78 and note.start not in note_times:
+#         extra_note = pretty_midi.Note(velocity=100, pitch=random.randint(71,75), start=note.start, end=end)
+#         output.instruments[0].notes.append(extra_note)
+#     # strum = pretty_midi.Note(velocity=100, pitch=78, start=note.start, end=end)
+#     output.instruments[0].notes.append(new_note)
+#     # output.instruments[0].notes.append(strum)
+
+# print(f"Total notes: {total}")
+# print(f"Out of range notes: {outofrange}")
+
+# output.write(f"{output_dir}/{song_name}/notes.mid")
+
+# output = mido.MidiFile(f"{output_dir}/{song_name}/notes.mid")
+# output.tracks[1].pop(1)
+# output.save(f"{output_dir}/{song_name}/notes.mid")
+
+# # write ini file
+# config = configparser.ConfigParser()
+# config.read('./song.ini')
+# config.set("song", "name", song_name.split(" - ")[1])
+# config.set("song", "artist", song_name.split(" - ")[0])
+# config.set("song", "charter", "Tim and Matthew")
+
+# with open(f"{output_dir}/{song_name}/song.ini", 'w') as configfile:    # save
+#     config.write(configfile)
