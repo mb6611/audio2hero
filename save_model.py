2e27fbc6b5a009e16ac4e43e6aab223679adfeb4
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2024-05-16 15:14:57 -0600

    Clean Up

--- save_model.py
+++
@@ -1,5 +0,0 @@
-from transformers import Pop2PianoForConditionalGeneration
-
-if __name__ == "__main__":
-    model = Pop2PianoForConditionalGeneration.from_pretrained("./audio2hero_adafactor_340")
-    model.push_to_hub("audio2hero")
